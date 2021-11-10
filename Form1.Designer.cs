02cade59c883abd044fa9b511e2a6bc6b2abcef7
VictorvanDen <qwe1234567890qweqwe@yandex.ru>
2021-11-10 14:22:19 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿
+namespace laba1_AIS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(17, 16);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(451, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1: Генерация ряда в label 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 48);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Изначальный массив";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(17, 137);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(451, 28);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2: Первый тип сортировки (вставки)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 169);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(520, 135);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(289, 28);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(17, 263);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(451, 28);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "button4: Второй тип сортировки (пузырек)";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(520, 263);
+            this.button5.Margin = new System.Windows.Forms.Padding(4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(289, 28);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 295);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(527, 295);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "label4";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(17, 404);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(451, 23);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 434);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(917, 409);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 17);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "label6";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections.Generic;
+
+namespace laba1_AIS
+{
+    public partial class Form1 : Form
+    {
+        //StreamReader sr = new StreamReader("C:\\Sample.txt");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        const int quantity = 10;
+        int[] massiv_in_begin = new int[quantity];
+        int[] massiv_in_begin1 = new int[quantity];
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string q = "";
+            label1.Text = q;
+            Random r = new Random();
+            for (int i = 0; i < quantity; i++) {
+                int y = r.Next(1, 1000);
+                massiv_in_begin[i] = y;
+                label1.Text = label1.Text + " " + Convert.ToString(y);
+                massiv_in_begin1[i] = massiv_in_begin[i];
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //int min = massiv_in_begin[0];
+            int u;
+            for (int i = 1; i < quantity + 1; i++) {
+                for (int k = 0; k < quantity; k++) {
+                    if (massiv_in_begin1[k] < massiv_in_begin1[i - 1])
+                    {
+                        //u = massiv_in_begin1[k];
+                        //massiv_in_begin1[k] = massiv_in_begin1[i - 1];
+                        //---min = massiv_in_begin[k];
+                        //massiv_in_begin1[i - 1] = u;
+                        Swap(ref massiv_in_begin1[k], ref massiv_in_begin1[i - 1]);
+                    }
+                }
+            }
+            string q = "";
+            for(int i = 0; i < quantity; i++)
+            {
+                q = q + " " + Convert.ToString(massiv_in_begin1[i]);
+            }
+            label3.Text = q;
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string q = "";
+            for (int i = 0; i < quantity; i++)
+            {
+                q = q + " " + Convert.ToString(massiv_in_begin1[i]);
+            }
+            label4.Text = q;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            List<int> list = massiv_in_begin.ToList();
+            QuickSort(list, 0, massiv_in_begin.Length);
+            printLab(label5, list.ToArray());
+            
+        }
+        public void QuickSort(List<int> list, int low, int high)
+        {
+            if(high - low > 1)
+            {
+                int m = list.Count();
+                label6.Text = Convert.ToString(m);
+                Random r = new Random();
+                int t = r.Next(low, high);
+                List<int> LowerT = new List<int>();
+                List<int> HigherT = new List<int>();
+                for (int i = t; i < high; i++)
+                {
+                    if (list[t] > list[i])
+                    {
+                        LowerT.Add(list[i]);
+                    }
+                    //else if (list[t] == list[i]) LowerT.Add(list[i]);
+                    else 
+                        HigherT.Add(list[i]);
+                }
+                //LowerT.Add(list[t]);
+                for(int i = 0; i < HigherT.Count(); i++)
+                {
+                    LowerT.Add(HigherT[i]);
+                }
+                list = LowerT;
+                QuickSort(list, low, t - 1);
+                QuickSort(list, t + 1, high);
+                
+            }
+            /*if(low < high)
+            {
+                int p = election(list, low, high);
+                QuickSort(list, low, p - 1);
+                QuickSort(list, p + 1, high);
+            }*/
+            /*if(high - low > 1)
+            {
+                int t;
+                if(high - low >= 3)
+                {
+                    Random r = new Random();
+                    t = r.Next(low + 1, high - 1);
+                    if(list[t + 1] < list[t] || list[t] < list[t - 1])
+                    {
+                        int b = list[t + 1];
+                        list[t + 1]
+                            = list[t - 1];
+                        list[t - 1] = b;
+                    }
+                    
+                    QuickSort(list, low, t - 1);
+                    QuickSort(list, t + 1, high);
+                }
+                else if(high - low == 2)
+                {
+                    if(list[low] > list[high]) {
+                        int b = list[low];
+                        list[low]
+                            = list[high];
+                        list[low] = b;
+                    }
+                }
+            }*/
+        }
+        public int election(List<int> A, int l, int h)
+        {
+            /*int o;
+            int pivot = A[h];
+            int i = l;
+            for(int j = l; j < h - 1; j++)
+            {
+                if(A[j] <= pivot)
+                {
+                    o = A[i];
+                    A[i] = A[j];
+                    A[j] = o;
+                    i++;
+                }
+            }
+            o = A[i];
+            A[i] = A[h];
+            A[h] = o;*/
+
+            Random r = new Random();
+            int i = r.Next(l, h);
+            return i;
+        }
+        public void Swap(ref int element1, ref int element2)
+        {
+            int boof = element1;
+            element1 = element2;
+            element2 = boof;
+        }
+        public void printLab(Label Lab, int[] mass)
+        {
+            string q = "";
+            for (int i = 0; i < quantity; i++)
+            {
+                q = q + " " + Convert.ToString(mass[i]);
+            }
+            Lab.Text = q;
+        }
+    }
+}
